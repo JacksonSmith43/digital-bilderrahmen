ddccf2e8975401fb0b8339852b9b96dcb09b04d9
JacksonSmith43 <jessica444@gmx.at>
2025-06-30 15:01:30 +0200

    Removed some dependencies. Added an empty (almost) upload component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GalleryComponent } from "./gallery/gallery.component";
+import { DragDropUploadComponent } from "./drag-drop-upload/drag-drop-upload.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [GalleryComponent],
+  imports: [GalleryComponent, DragDropUploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/drag-drop-upload/drag-drop-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-drop-upload',
+  imports: [],
+  templateUrl: './drag-drop-upload.component.html',
+  styleUrl: './drag-drop-upload.component.css'
+})
+export class DragDropUploadComponent {
+
+}
