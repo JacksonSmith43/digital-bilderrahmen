f017330ccfc970e866ed8f5858beba491c8ffd0b
JacksonSmith43 <jessica444@gmx.at>
2025-06-30 14:37:54 +0200

    Made the images dynamic.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 
 export class GalleryComponent {
 
+  images = [
+    { src: "assets/assassins-creed.jpg", alt: "Assassin´s-creed logo." },
+    { src: "assets/car.jpg", alt: "Cool looking car." },
+    { src: "assets/guinea-pig.jpg", alt: "A Guinea Pig lifting weights." },
+    { src: "assets/hamsterviel.bmp", alt: "Hamsterviel laughing evily." },
+    { src: "assets/snowman.JPG", alt: "A person standing behind a devil looking snowman." },
+  ]
 }
