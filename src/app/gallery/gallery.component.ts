b3f3d9c0e41acf8b5edfa9d5c16b3173423c30c7
JacksonSmith43 <jessica444@gmx.at>
2025-07-02 11:06:14 +0200

    Added images are appended to the Gallery.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { GalleryService } from './gallery.service';
 import { DragDropUploadService } from '../drag-drop-upload/drag-drop-upload.service';
 
@@ -14,6 +14,11 @@ export class GalleryComponent {
   private dragDropUploadService = inject(DragDropUploadService);
 
   images = this.galleryService.images;
+  imagesSignal = signal(this.images);
   addedImages = this.dragDropUploadService.images;
+
+  allImages = computed(() => [
+    ...this.imagesSignal(), ...this.addedImages().map(src => ({ src, alt: "" })) // Combines the images from the gallery and the added images by mapping each image URL to an object with src and alt properties. addedImages will be addded to imagesSignal when a new image is added via drag and drop upload.
+  ]);
 
 }
