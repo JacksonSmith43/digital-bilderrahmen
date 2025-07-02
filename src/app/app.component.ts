78273746902d22b85599fe21970273df3f5357d5
JacksonSmith43 <jessica444@gmx.at>
2025-07-02 12:13:42 +0200

    Able to view specific features (only pictures etc.).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { GalleryComponent } from "./gallery/gallery.component";
 import { DragDropUploadComponent } from "./drag-drop-upload/drag-drop-upload.component";
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [GalleryComponent, DragDropUploadComponent],
+  imports: [GalleryComponent, DragDropUploadComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'bilderrahmen';
+  isOnlyPictures = false;
+  isDragDrop = false;
+  isAllView = true;
+  selectedView: "viewAll" | "viewPictures" | "viewDragDrop" = "viewAll";
+
+  onPicturesView() {
+    this.isOnlyPictures = true;
+    this.isAllView = false;
+    this.isDragDrop = false;
+    this.selectedView = "viewPictures"
+  }
+
+  onDragDropView() {
+    this.isOnlyPictures = false;
+    this.isAllView = false;
+    this.isDragDrop = true;
+    this.selectedView = "viewDragDrop"
+  }
+
+  onAllView() {
+    this.isOnlyPictures = false;
+    this.isAllView = true;
+    this.isDragDrop = false;
+    this.selectedView = "viewAll"
+  }
 }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: AppComponent },
+    { path: "gallery", component: GalleryComponent }
+];
