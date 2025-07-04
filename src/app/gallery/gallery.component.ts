aed6c64bf9e8bdb89f4545e03f51ac45a529a35d
JacksonSmith43 <jessica444@gmx.at>
2025-07-04 10:37:21 +0200

    Images can be removed.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -14,11 +14,29 @@ export class GalleryComponent {
   private dragDropUploadService = inject(DragDropUploadService);
 
   images = this.galleryService.images;
-  imagesSignal = signal(this.images);
   addedImages = this.dragDropUploadService.images;
 
   allImages = computed(() => [
-    ...this.imagesSignal(), ...this.addedImages() // Combines the images from both sources. 
+    ...this.images(), ...this.addedImages() // Combines the images from both sources. 
   ]);
+
+  onRemoveImage(index: number) {
+    const galleryLength = this.images().length;
+
+    if (index < galleryLength) { // Removes the hardcoded images. // 3 < 5 = Removes the image at the 3 index, so Hamsterviel. 
+      this.images.update((imageArray) => {
+        imageArray.splice(index, 1);
+        return [...imageArray];
+      })
+
+    } else { // Removes the uploaded images. 
+      const uploadIndex = index - galleryLength; // 3 - 5. 
+      this.addedImages.update((imageArray) => {
+        imageArray.splice(uploadIndex, 1);
+        return [...imageArray];
+      })
+    }
+  }
+
 
 }
--- src/app/gallery/gallery.service.ts
+++ src/app/gallery/gallery.service.ts
@@ -1,17 +1,14 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 
 @Injectable({ providedIn: "root" })
 export class GalleryService {
 
-    images = [
+    images = signal([
         { src: "assets/assassins-creed.jpg", alt: "Assassin´s-creed logo.", relativePath: "" },
         { src: "assets/car.jpg", alt: "Cool looking car.", relativePath: "" },
         { src: "assets/guinea-pig.jpg", alt: "A Guinea Pig lifting weights.", relativePath: "" },
         { src: "assets/hamsterviel.bmp", alt: "Hamsterviel laughing evily.", relativePath: "" },
         { src: "assets/snowman.JPG", alt: "A person standing behind a devil looking snowman.", relativePath: "" },
-    ]
+    ]);
 
-    getImageRemoval() {
-
-    }
 } 
