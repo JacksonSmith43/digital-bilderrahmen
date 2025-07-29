19943052919cb391220eb842c6347f5defcb4286
JacksonSmith43 <jessica444@gmx.at>
2025-07-29 16:16:17 +0200

    Able to upload all images to Firebase. Able to upload one image to the device selection in Firebase.

--- frontend/src/app/gallery/gallery-storage.service.ts
+++ frontend/src/app/gallery/gallery-storage.service.ts
@@ -2,34 +2,74 @@ import { inject, Injectable } from '@angular/core';
 import { doc, Firestore, setDoc } from '@angular/fire/firestore';
 import { ref, Storage, uploadBytesResumable, UploadTask } from '@angular/fire/storage';
 
-import { DragDropUploadService } from '../drag-drop-upload/drag-drop-upload.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryStorageService {
+  action: "selectForDevice" | "uploadAllImages" | undefined = undefined;
   firestore = inject(Firestore);
   firebaseStorage = inject(Storage);
 
-  uploadImageNames(filteredSrcs: string[]) {
+  uploadSingleImage(imageName: string, image: Blob, action: string | undefined): UploadTask {
+    if (!action) {
+      throw new Error('No action set for single upload.');
+    }
 
-    const imagesDocumentRef = doc(this.firestore, "images", this.createsSlug()); // Will create a new document called "images-<randomNumber>" in the "images" collection. 
-    setDoc(imagesDocumentRef, {
-      images: filteredSrcs,
+    console.log("uploadSingleImage()_action: ", action);
 
-    });
+    if (action === "uploadAllImages") {
+      const storageRef = ref(this.firebaseStorage, `uploadedImages/${imageName}`); // This will create a reference to the images folder in the storage bucket. 
+      return uploadBytesResumable(storageRef, image); // This will upload the image to the storage bucket. 
 
+    } else if (action === "selectForDevice") {
+      const storageRef = ref(this.firebaseStorage, `selectForDevice/${imageName}`);
+      return uploadBytesResumable(storageRef, image);
+
+    } else {
+      throw new Error('Invalid action for uploadSingleImage.');
+    }
   }
 
-  createsSlug() {
-    console.log("createsSlug().");
-    const randomNumber = Math.floor(Math.random() * 1000);
-    return `images-${randomNumber}`;
+
+  async urlToBlob(url: string): Promise<Blob> { // This will convert the URL to a blob. Works with both external URLs like "https://example.com/image.jpg" and local paths like "assets/car.jpg".
+    const response = await fetch(url); // This will fetch the image from the URL. This requests the image data from the server and loads it into memory. 
+    return await response.blob(); // Only once the image has been downloaded, will it be converted to a blob.
   }
 
-  uploadImages(imageName: string[], image: Blob | ArrayBuffer | Uint8Array): UploadTask {
-    const storageRef = ref(this.firebaseStorage, `images/${imageName}`);
-    return uploadBytesResumable(storageRef, image);
+  dataURLToBlob(dataURL: string): Blob { // This will convert the data URL to a blob. An example would be "data:image/jpeg;base64,/9j/4AAQSkZJRgABAg..."
+    const arr = dataURL.split(',');
+    const mime = arr[0].match(/:(.*?);/)![1];
+    const bstr = atob(arr[1]); // bstr is a binary string. 
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) { // This will loop through the binary string and convert it to a Uint8Array. n-- means that it will decrement the value of n by 1 and once it reaches 0, it will stop.
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime }); // Blob([u8arr], { type: mime }) means that [u8arr] is the data and { type: mime } is the type of the data. All in all this will convert the data URL to a blob. 
   }
-  
+
+  async convertSrcToBlob(src: string): Promise<Blob> {
+    let blob: Blob;
+
+    if (src.startsWith('data:')) {
+      blob = this.dataURLToBlob(src);
+
+    } else {
+      blob = await this.urlToBlob(src);
+    }
+    return blob;
+  }
+
+  async convertToBlobs(srcs: string[]): Promise<Blob[]> {
+    const blobs: Blob[] = [];
+
+    for (let src of srcs) {
+      let blob = await this.convertSrcToBlob(src);
+      blobs.push(blob);
+    }
+    console.log("convertToBlobs()_blobs: ", blobs);
+    return blobs;
+  }
+
 }
--- frontend/src/app/gallery/gallery.component.ts
+++ frontend/src/app/gallery/gallery.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { getDownloadURL } from '@angular/fire/storage';
 
 import { GalleryService } from './gallery.service';
 import { GalleryStorageService } from '../gallery/gallery-storage.service';
 import { DragDropUploadService } from '../drag-drop-upload/drag-drop-upload.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -16,16 +16,17 @@ import { DragDropUploadService } from '../drag-drop-upload/drag-drop-upload.serv
 export class GalleryComponent implements OnInit {
   private galleryService = inject(GalleryService);
   private galleryStorageService = inject(GalleryStorageService);
+  dragDropUploadService = inject(DragDropUploadService);
+
 
   allImages = this.galleryService.allImages;
   galleryHighlightSrcs = this.galleryService.galleryHighlightSrcs;
   notDeletedImagesArray = this.galleryService.notDeletedImagesArray;
   imagesLength = this.galleryService.imagesLength;
+  files = this.dragDropUploadService.files;
+  images = this.dragDropUploadService.images;
 
-  dragDropUploadService = inject(DragDropUploadService);
-  
-    files = this.dragDropUploadService.files;
-    images = this.dragDropUploadService.images;
+  action = this.galleryStorageService.action;
 
   ngOnInit() {
     this.galleryService.notDeletedImages();
@@ -52,6 +53,7 @@ export class GalleryComponent implements OnInit {
 
   onSelectForDevice() {
     console.log("onSelectForDevice().");
+    this.action = "selectForDevice";
 
     const selectedSrcs = this.galleryService.galleryHighlightSrcs();
     const existingRaw = localStorage.getItem("chosenImagesSrcs");
@@ -62,34 +64,52 @@ export class GalleryComponent implements OnInit {
     const availableImageSrcs = this.galleryService.notDeletedImagesArray().map(img => img.src); // This will return an array of the srcs of the images that are not deleted. 
     const filteredSrcs = combinedSrcs.filter(src => !deletedSrcs.includes(src) && availableImageSrcs.includes(src)); // This will filter out the deleted images and the images that are not available.
 
+    const imageNames = filteredSrcs.map(src =>
+      src.substring(src.lastIndexOf('/') + 1) // This will return the name of the image. lastIndexOf('/') + 1 means that the first character after the last / will be returned. 
+    );
+
     localStorage.setItem("chosenImagesSrcs", JSON.stringify(filteredSrcs));
     console.log("onSelectForDevice()_combinedSrcs: ", combinedSrcs);
     console.log("onSelectForDevice()_filteredSrcs: ", filteredSrcs);
+    console.log("onSelectForDevice()_typeoffilteredSrcs: ", typeof filteredSrcs);
 
     this.galleryService.galleryHighlightSrcs.set([]);
 
-    this.galleryStorageService.uploadImageNames(filteredSrcs);
-    this.selectedImage();
+    if (filteredSrcs.length > 0) { // Checks if the filteredSrcs array is empty. 
+      this.galleryStorageService.convertToBlobs(filteredSrcs[0]) // [filteredSrcs[0]] creates an array with the first element of the filteredSrcs array because this methode requires an array. 
+        .then((blobs: Blob[]) => { // The result of the convertToBlobs method is stored in the blobs variable which is an array of Blobs.
+          this.galleryStorageService.uploadSingleImage(imageNames[0], blobs[0], this.action);
+          console.log("onSelectForDevice()_blobs: ", blobs);
+        });
+    }
   }
 
 
-  selectedImage() {
+  async onUploadAllImages() {
+    console.log("onUploadAllImages().");
 
-    const imageNames = this.images().map(img => img.relativePath); 
-    const ngxFileDropEntries = this.files(); // This will return an array of the files that were dropped. 
-    
-    if (ngxFileDropEntries.length > 0) {
-      const fileEntry = ngxFileDropEntries[0].fileEntry as FileSystemFileEntry;
-      fileEntry.file((file: File) => {
-        this.galleryStorageService.uploadImages(imageNames, file)
-          .then((snapshot) => {
-            getDownloadURL(snapshot.ref)
-              .then((downloadUrl) => {
-                console.log("selectedImage()_downloadUrl: ", downloadUrl);
-              })
-          })
-      });
+    this.action = "uploadAllImages";
+    const images = this.galleryService.allImages();
+    let imageName: string = "";
+    console.log("onUploadAllImages()_images: ", images);
+
+    for (let img of images) {
+      let uploadTasks: any[] = await this.galleryStorageService.convertToBlobs([img.src]);
+      console.log("onUploadAllImages()_uploadTasks: ", uploadTasks);
+
+      if (img.alt === "") {
+        imageName = img.relativePath;
+
+      } else if (img.relativePath === "") {
+        imageName = img.alt;
+
+      } else {
+        imageName = `image_${Date.now()}_${img}`; // Incase both the alt and relativePath are empty. 
+      }
+
+      await this.galleryStorageService.uploadSingleImage(imageName, uploadTasks[0], this.action);
     }
+    this.galleryService.galleryHighlightSrcs.set([]); // Incase any images are selected. 
   }
 
 }
